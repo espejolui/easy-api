b736223a969f48c86ef42987cc813bb988671f3d
Luis Espejo <herrera.espejo.luis@gmail.com>
2024-06-28 14:52:57 -0500

    Create route recipes

---
+++ src/routes/recipes.routes.ts
@@ -0,0 +1,8 @@
+// Utilidad para manejar las rutas
+import { Router } from "express";
+import { recipesControllers } from "../controllers/recipes.controllers";
+
+const router = Router();
+// Cuando se enrute a /recipes devuelve la respuesta del controlador recipes
+router.get("/recipes", recipesControllers);
+export default router;
