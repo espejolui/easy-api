58cc52f217a27c525aab213249d599b0cdb9ce70
Luis Espejo <herrera.espejo.luis@gmail.com>
2024-07-09 22:58:55 -0500

    Changed query DB

--- src/controllers/recipes.controllers.js
+++ src/controllers/recipes.controllers.js
@@ -3,6 +3,8 @@ import { pool } from "../db.js";
 export const getRecipes = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
 
-  const [response] = await pool.query("SELECT * FROM fruits");
+  const [response] = await pool.query(
+    "SELECT r.id, r.title, r.preparation, d.day_week, t.type_food FROM recipe r JOIN day d ON r.day_id = d.id JOIN type t ON r.type_id = t.id;"
+  );
   res.json(response);
 };
