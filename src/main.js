831fc2e3b8661b71720be1e1b5062bfe9636e5f6
Luis Espejo <herrera.espejo.luis@gmail.com>
2024-07-09 22:58:13 -0500

    Changed port to 4000

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { app } from "./app.js";
+// 1. Importando express para usarlo
+import express from "express";
 
-const PORT = 3000;
+// 2. Importando rutas creadas para ser usadas
+import recipes from "./routes/recipes.routes.js";
+
+// 3. Creando constante para escuchar en un puerto leventar el servidor
+const app = express();
+
+// 4. Rutas importadas | añadiendo palabra "api" antes de dicha ruta simplemente por conveción
+app.use("/api", recipes);
+
+// 5. Creando el el puerto y diciendole que escuche por él
+const PORT = 4000;
 app.listen(PORT);
-console.log(`Escuchando en el pueto ${PORT}`);
+console.log(`Listen in port: ${PORT}`);
