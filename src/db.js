20f0cfede498e904d88499ec6b8bde73bac1997b
Luis Espejo <herrera.espejo.luis@gmail.com>
2024-07-10 09:46:45 -0500

    Usando variables de entorno para conectar DB

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
 // 1. Promise para poder manejar el async await
 import { createPool } from "mysql2/promise";
 
+process.loadEnvFile();
+
 // 2. Configuración con los datos de mi db usando el objeto creayePool de Express
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "Camaro@3186",
-  port: 33061,
-  database: "easyfood",
+  host: process.env.HOST_DEV,
+  user: process.env.USER_DEV,
+  password: process.env.PASSWORD_DEV,
+  port: process.env.PORT_DEV,
+  database: process.env.DATABASE_DEV,
 });
-
-
