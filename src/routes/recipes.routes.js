7fa88d08e96db8379c65841e1bed30c98f4cc4ba
lucho <herrera.espejo.luis@gmail.com>
2024-08-11 21:15:08 -0500

    CRUD completo

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -1,11 +1,22 @@
 // 1. Utilidad para manejar las rutas
 import { Router } from "express";
-// 2. Usando el controlador que me brinda la respuesta
-import { getRecipes } from "../controllers/recipes.controllers.js";
+import { pool } from "../db.js"; // 2. usando el objeto pool de pg
+import {
+  getRecipes,
+  getRecipe,
+  createRecipe,
+  deleteRecipe,
+  updateRecipe,
+} from "../controllers/recipes.controllers.js";
 
-// 3. Constante para esportar las difeentes rutas con ayuda del modulo Routes
+// 3. Constante para esportar las diferentes rutas con ayuda del modulo Routes
 const router = Router();
 
 // 4. Cuando se consulte /recipes devuelve la respuesta del controlador recipesControllers
 router.get("/recipes", getRecipes);
+router.get("/recipes/:id", getRecipe);
+router.post("/recipes", createRecipe);
+router.put("/recipes/:id", updateRecipe);
+router.delete("/recipes/:id", deleteRecipe);
+
 export default router;
