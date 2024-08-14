9d7d1f18b4587e8515ab66cc2f18df714493392c
lucho <herrera.espejo.luis@gmail.com>
2024-08-14 16:45:04 -0500

    Organización del script para crear la db

--- data/db_pg.sql
+++ data/db_pg.sql
@@ -1,21 +1,19 @@
--- Crear base de datos
+-- 1. Crear base de datos
 CREATE DATABASE easyfood;
 
--- Conectar a la base de datos
+-- 2. Conectarse a la base de datos
 \c easyfood
 
--- ---------------   1   ---------------
-
--- Eliminar la tabla si existe
+-- 2. Eliminar la tabla si existe
 DROP TABLE IF EXISTS day;
 
--- Crear la tabla 'day'
+-- 2.1. Crear la tabla 'day'
 CREATE TABLE day (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     day_week VARCHAR(10) NOT NULL UNIQUE
 );
 
--- Insertar datos en la tabla 'day'
+-- 2.2. Insertar datos en la tabla 'day'
 INSERT INTO day(day_week) VALUES
     ('lunes'),
     ('martes'),
@@ -23,35 +21,31 @@ INSERT INTO day(day_week) VALUES
     ('jueves'),
     ('viernes');
 
--- Leer datos de la tabla 'day'
+-- 2.3. Leer datos de la tabla 'day'
 SELECT id, day_week FROM day;
 
--- ---------------   2   ---------------
-
--- Eliminar la tabla si existe
+-- 3. Eliminar la tabla si existe
 DROP TABLE IF EXISTS type;
 
--- Crear la tabla 'type'
+-- 3.1. Crear la tabla 'type'
 CREATE TABLE type (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     type_food VARCHAR(10) NOT NULL UNIQUE
 );
 
--- Insertar datos en la tabla 'type'
+-- 3.2. Insertar datos en la tabla 'type'
 INSERT INTO type(type_food) VALUES
     ('desayuno'),
     ('almuerzo'),
     ('cena');
 
--- Leer datos de la tabla 'type'
+-- 3.3. Leer datos de la tabla 'type'
 SELECT id, type_food FROM type;
 
--- ---------------   3   ---------------
-
--- Eliminar la tabla si existe
+-- 4. Eliminar la tabla si existe
 DROP TABLE IF EXISTS recipe;
 
--- Crear la tabla 'recipe'
+-- 4.1. Crear la tabla 'recipe'
 CREATE TABLE recipe (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -63,7 +57,7 @@ CREATE TABLE recipe (
     FOREIGN KEY (type_id) REFERENCES type(id)
 );
 
--- Insertar datos en la tabla 'recipe'
+-- 4.2. Insertar datos en la tabla 'recipe'
 INSERT INTO recipe(
     title, preparation, photo, day_id, type_id)
 VALUES
@@ -73,27 +67,23 @@ VALUES
     (SELECT id FROM day WHERE day_week = 'lunes'),
     (SELECT id FROM type WHERE type_food = 'desayuno'));
 
--- Leer datos de la tabla 'recipe'
+-- 4.3 Leer datos de la tabla 'recipe'
 SELECT id, title FROM recipe;
 
--- Leer datos de la tabla 'recipe' con joins
+-- 4.4 Leer datos de la tabla 'recipe' con joins
 SELECT r.id, r.title, d.day_week, t.type_food
 FROM recipe r
 JOIN day d ON r.day_id = d.id
 JOIN type t ON r.type_id = t.id;
 
--- ---------------   4   ---------------
-
--- Crear la tabla 'ingredient'
+-- 5. Crear la tabla 'ingredient'
 CREATE TABLE ingredient (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     amount VARCHAR(50) NOT NULL
 );
 
--- ---------------   5   ---------------
-
--- Crear la tabla 'recipe_ingredient'
+-- 6. Crear la tabla para relación tabla "recipe" con tabla "ingresient"
 CREATE TABLE recipe_ingredient (
     recipe_id INT,
     ingredient_id INT,
