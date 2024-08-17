435964a3da38ed4fd798865e1ca1a38ddf6aede4
lucho <herrera.espejo.luis@gmail.com>
2024-08-17 10:48:35 -0500

    corrección de rutas

--- src/routes/recipes.routes.js
+++ src/routes/recipes.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"; // 1. Método para manejar las rutas
-import { getRecipes, getRecipe } from "../controllers/get.js"
+import { getRecipes, getRecipe, getRecipeIngredient } from "../controllers/get.js"
 import { createRecipe } from "../controllers/create.js";
+import { addIngredientsRecipe } from "../controllers/relationAdd.js";
 import { deleteRecipe } from "../controllers/delete.js";
 import { updateRecipe,} from "../controllers/update.js";
 
@@ -8,7 +9,11 @@ const router = Router(); // 2. almacenando rutas en la variable router
 
 router.get("/recipes", getRecipes);
 router.get("/recipes/:id", getRecipe);
+router.get("/recipes/join", getRecipeIngredient);
+
 router.post("/recipes", createRecipe);
+router.post("/recipes/relation-add", addIngredientsRecipe);
+
 router.put("/recipes/:id", updateRecipe);
 router.delete("/recipes/:id", deleteRecipe);
 
