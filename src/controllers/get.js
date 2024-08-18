2ed037d9cb123c360cb292dd20914463884932e7
lucho <herrera.espejo.luis@gmail.com>
2024-08-17 21:14:45 -0500

    Función para consultar datos y usarlos en el front

--- src/controllers/get.js
+++ src/controllers/get.js
@@ -1,13 +1,64 @@
 import { pool } from "../db.js";
 
-// 1. Obtener todas las recetas
-export const getRecipes = async (req, res) => {
+// Middleware para CORS (si es necesario)
+export const corsMiddleware = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-
-  const { rows } = await pool.query(
-    "SELECT id, title, preparation, photo FROM recipe;",
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
   );
-  res.json(rows);
+  next();
+};
+
+// Obtener todas las recetas con sus ingredientes agrupados
+export const getRecipes = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query; // Obtener parámetros de paginación
+
+  const offset = (page - 1) * limit;
+
+  try {
+    const { rows } = await pool.query(
+      `SELECT
+         r.title,
+         r.photo,
+         i.name AS ingredient_name,
+         u.name AS unit,
+         riu.quantity
+       FROM recipe r
+       JOIN recipe_ingredient_unit riu ON r.id = riu.recipe_id
+       JOIN ingredient i ON riu.ingredient_id = i.id
+       JOIN unit u ON riu.unit_id = u.id
+       LIMIT $1 OFFSET $2`,
+      [limit, offset],
+    );
+
+    // Agrupar ingredientes por receta
+    const recipes = {};
+
+    rows.forEach((row) => {
+      if (!recipes[row.name_recipe]) {
+        recipes[row.name_recipe] = {
+          title: row.title,
+          photo: row.photo,
+
+          ingredients: [],
+        };
+      }
+      recipes[row.name_recipe].ingredients.push({
+        ingredient_name: row.ingredient_name,
+        unit: row.unit,
+        quantity: row.quantity,
+      });
+    });
+
+    // Convertir el objeto de recetas en un array
+    const result = Object.values(recipes);
+
+    res.json(result);
+  } catch (err) {
+    console.log("Error al obtener recetas:", err);
+    res.status(500).json({ mensaje: "Error interno del servidor" });
+  }
 };
 
 // 2. Obtener una receta por su id
